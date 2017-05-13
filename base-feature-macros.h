9daa3146fd7812ee42142b0536592149b4011459
Herbert Valerio Riedel <hvr@gnu.org>
2017-05-13 09:59:09 +0200

    Initial version

---
+++ base-feature-macros.h
@@ -0,0 +1,26 @@
+#if !defined(HS_BASE_FEATURE_MACROS_H)
+#define HS_BASE_FEATURE_MACROS_H
+
+#if !defined(MIN_VERSION_base)
+#error MIN_VERSION_base macro not defined
+#endif
+
+/* @since 0.1 */
+#define HAVE_APPLICATIVE_MONAD                  MIN_VERSION_base(4,8,0)
+
+/* @since 0.1 */
+#define HAVE_FOLDABLE_TRAVERSABLE_IN_PRELUDE    MIN_VERSION_base(4,8,0)
+
+/* @since 0.1 */
+#define HAVE_MONOID_IN_PRELUDE                  MIN_VERSION_base(4,8,0)
+
+/* @since 0.1 */
+#define HAVE_NATURAL_IN_BASE                    MIN_VERSION_base(4,8,0)
+
+/* @since 0.1 */
+#define HAVE_SEMIGROUP_IN_BASE                  MIN_VERSION_base(4,8,0)
+
+/* @since 0.1 */
+#define HAVE_MONAD_FAIL                         MIN_VERSION_base(4,9,0)
+
+#endif
